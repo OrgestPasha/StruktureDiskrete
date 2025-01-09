670f3747a1857f0bd7ad09d39364cca6df1948d3
OrgestPasha <orgestpasha2005@gmail.com>
2025-01-09 21:37:09 +0100

    Full Project

---
+++ erastoteni.js
@@ -0,0 +1,46 @@
+function calculate() {
+  //get n from the input
+  const inputElement = document.getElementById("numberInput");
+  const n = parseInt(inputElement.value, 10);
+
+  //get sqr root of n
+  let rootOfN = Math.floor(Math.sqrt(n));
+
+  //declare array
+  let array = [];
+
+  //fill up array
+  for (let x = 0; x <= n; x++) {
+    array[x] = x;
+  }
+
+  //mark non prime numbers as null
+  for (let x = 2; x <= rootOfN; x++) {
+    if (array[x] != null) {
+      for (let y = x * x; y <= n; y += x) {
+        array[y] = null;
+      }
+    }
+  }
+  //filter out non prime numbers and log the result
+
+  array = array.filter((num) => num != null && num >= 2);
+
+  let container = document.getElementById("number-container");
+  container.innerHTML = "";
+  for (let x = 0; x < array.length; x++) {
+    setTimeout(() => {}, 1000);
+  }
+  let i = 0;
+  const a = () => {
+    if (i < array.length) {
+      var element = document.createElement("p");
+      element.appendChild(document.createTextNode(array[i++]));
+      container.appendChild(element);
+    }
+    setTimeout(() => {
+      a();
+    }, 50);
+  };
+  a();
+}
